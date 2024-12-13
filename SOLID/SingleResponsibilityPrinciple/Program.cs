bdc198b270d28884588566aabe10485a2161818e
nixhantb <nishantbanjade07@gmail.com>
2024-12-13 20:42:30 +0545

    initial commit

---
+++ SOLID/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,96 @@
+﻿
+namespace SingleResponsibilityPrinciple
+{
+
+    // Logger: (SRP: Handles logging logic)
+
+    public class Logger
+    {
+
+        private readonly FileManager _fileManager;
+
+        public Logger(FileManager fileManager)
+        {
+            _fileManager = fileManager;
+        }
+
+
+        public void LogInfo(string message)
+        {
+
+            var logMessage = $"{DateTime.Now} [INFO] : {message}";
+            _fileManager.WriteToFile(logMessage);
+        }
+
+        public void LogError(string message)
+        {
+            var logMessage = $"{DateTime.Now} [ERROR]: {message}";
+            _fileManager.WriteToFile(logMessage);
+        }
+    }
+
+    // Handles file Operations (SRP)
+    public class FileManager
+    {
+
+        private readonly string _filePath;
+
+        public FileManager(string filePath)
+        {
+
+            _filePath = filePath;
+            EnsureFileExists();
+        }
+
+        private void EnsureFileExists()
+        {
+
+            var directory = Path.GetDirectoryName(_filePath);
+           
+
+            if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+            {
+
+                Directory.CreateDirectory(directory);
+            }
+
+            if (!File.Exists(_filePath))
+            {
+                File.Create(_filePath).Dispose();
+            }
+
+        }
+
+        public void WriteToFile(string content)
+        {
+            File.AppendAllText(_filePath, content + Environment.NewLine);
+        }
+
+    }
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var logger = new Logger(new FileManager("application.log"));
+
+            logger.LogInfo("Application started");
+
+            try
+            {
+                throw new InvalidOperationException("Something went wrong");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+            }
+
+            logger.LogInfo("Application ended.");
+            Console.WriteLine("Logs written successfully!");
+        }
+    }
+
+
+}
---
+++ SOLID/SingleResponsibilityPrinciple/README.md
@@ -0,0 +1,11 @@
+## Single Responsibility Principle
+
+
+    
+Any particular class should have single reason to change meaning it should have only one well-defined responsibility or behavior.
+    
+SRP means breaking down the functionality into separate classes based on their distinct responsibilities. One class should handle the task of saving strings in a list (e.g., an "ItemList" class with methods for adding, removing, and updating items), while another class should handle the task of saving the list items to a file.txt (e.g., a "FileHandler" class). This way, each class has a single reason to change and is responsible for a specific aspect of the functionality, leading to a more maintainable and modular codebase.
+    
+By **separating concerns** in this manner, you not only improve code organization but also make it easier to make changes or enhancements in the future. Changes related to file handling won't impact the logic of adding/removing items from the list, and vice versa, reducing the risk of introducing unintended bugs.
+    
+For example, consider a class that handles both user authentication and database connection. If there is a change in the authentication mechanism, it could affect the database connection logic and vice versa. In this case, the class violates the SRP. To adhere to the SRP, you would create separate classes for user authentication and database connection, each with a single responsibility. This way, changes in one area won't affect the other, making the codebase more maintainable and less error-prone.
