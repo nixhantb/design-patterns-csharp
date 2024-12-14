3601a660cf38d61a7409072c626fb9ac5ede71b2
nixhantb <nishantbanjade07@gmail.com>
2024-12-14 13:41:07 +0545

    add LSP

---
+++ SOLID/LiscovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace LiscovSubstitutionPrinciple
+{
+
+    public class Rectangle
+    {
+
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
+    public class Program
+    {
+
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has Area {Area(rc)}");
+
+            Rectangle sq = new Square();
+
+            sq.Height = 5;
+
+            Console.WriteLine($"{sq} has Area {Area(sq)}");
+
+        }
+    }
+}
---
+++ SOLID/LiscovSubstitutionPrinciple/README.md
@@ -0,0 +1,11 @@
+## Liscov Substitution Principle
+
+The Liskov Substitution Principle (LSP) is the "L" in the SOLID principles. It states:
+
+"Subtypes must be substitutable for their base types."
+
+In simpler terms, objects of a subclass should be able to replace objects of the parent class without affecting the correctness of the program.
+
+In more simple terms, it states that objects of a superclass should be replaceable with objects of a subclass without affecting the correctness of the program.
+
+To put it even simpler, if a class (subclass) is derived from another class (superclass), any instance of the superclass should be able to be replaced with an instance of the subclass without causing any unexpected behavior or breaking the code.
